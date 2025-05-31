4b1d642708b90733e2a2acebf98a7076ca813a47
Robert Habermeier <rphmeier@gmail.com>
2025-05-31 17:10:38 -0500

    remove validation at beginning

--- main.py
+++ main.py
@@ -557,9 +557,6 @@ def main():
 
     print(f"Optimizer dtype: {next(iter(optimizer.param_groups[0]['params'])).dtype}")
 
-    # TODO: remove. this just is for testing
-    validate(transformer, val_dataloader, accelerator, pipeline, epoch=0, offload_heavy=args.offload_heavy_encoders)
-
     transformer.train()
     for epoch in range(args.epochs):
         if accelerator.is_main_process:
